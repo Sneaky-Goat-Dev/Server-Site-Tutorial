d428742ca07a179c250bd21fdcfbcb707a44a7c8
Sneaky-Goat-Dev <bergeradam20@gmail.com>
2024-05-07 23:21:08 +0200

    Push Changes to Github

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config({ path: "../.env" });
+
+const basePath = path.join(__dirname, "..", "site");
 
 const app = express();
 
@@ -10,6 +13,10 @@ app.use(express.json());
 const userRoutes = require("./api/userRoutes");
 
 app.use("/api", userRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(basePath, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
